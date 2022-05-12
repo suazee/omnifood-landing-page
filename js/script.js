8cec1b727b13e340102e8c15041b07092e3b50a4
Johnson Tolefe <96609596+suazee@users.noreply.github.com>
2022-05-12 20:58:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,13 @@ const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 // Make mobile navigation work
+const html = document.querySelector("html");
 const headerEl = document.querySelector(".header");
 const navMenuBtn = document.querySelector(".nav-menu-btn");
 
 navMenuBtn.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+  html.classList.toggle("prevent-scroll");
 });
 
 // Smooth scroll animation
@@ -37,6 +39,7 @@ allLinks.forEach((link) => {
     // Close mobile navigation
     if (link.classList.contains("main-nav-item")) {
       headerEl.classList.toggle("nav-open");
+      html.classList.toggle("prevent-scroll");
     }
   });
 });
